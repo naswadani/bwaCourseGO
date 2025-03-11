dd1605ef710023c4459e078cc681530d88e6a90f
Naswa Bryna Danikhansa <naswakhansa23@gmail.com>
2025-03-11 20:40:33 +0700

    make login session

--- main.go
+++ main.go
@@ -21,12 +21,12 @@ func main() {
 	userRepository := user.NewRepository(db)
 	userService := user.NewService(userRepository)
 
-	userByEmail, err := userRepository.FindByEmail(" ")
-
 	userHandler := handler.NewUserHandler(userService)
 	router := gin.Default()
 	api := router.Group("/api/v1")
-	api.POST("/users", userHandler.RegisterUser)
-	router.Run()
 
+	api.POST("/users", userHandler.RegisterUser)
+	api.POST("/sessions", userHandler.Login)
+
+	router.Run()
 }
--- user/input.go
+++ user/input.go
@@ -11,3 +11,8 @@ type LoginUserInput struct {
 	Email    string `json:"email" binding:"required,email"`
 	Password string `json:"password" binding:"required"`
 }
+
+type LoginInput struct {
+	Email    string `json:"email" binding:"required,email"`
+	Password string `json:"password" binding:"required"`
+}
